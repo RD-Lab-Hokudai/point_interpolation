a8113038aba7e5d949dab6ebe6a16a7758193ab1
sono8stream <sono.stream8580@gmail.com>
2020-07-30 22:21:45 +0900

    Add PWAS acceleration method

--- NeighborTest.cpp
+++ NeighborTest.cpp
@@ -314,6 +314,8 @@ struct misra1a_functor2 : Functor<double>
     int values() const { return values_; }
 };
 
+ofstream ofs("res_original.csv");
+
 const int width = 938;
 const int height = 606;
 //const int width = 882;
@@ -322,21 +324,23 @@ const double f_x = width / 2 * 1.01;
 
 // Calibration
 // 02_04_13jo
-
+/*
 int X = 498;
 int Y = 485;
 int Z = 509;
-int theta = 483;
-int phi = 518;
-
+int roll = 481;
+int pitch = 517;
+int yaw = 500;
+*/
 // 02_04_miyanosawa
-/*
+
 int X = 495;
 int Y = 475;
 int Z = 458;
-int theta = 438;
-int phi = 512;
-*/
+int roll = 488;
+int pitch = 568;
+int yaw = 500;
+
 // 03_03_miyanosawa
 /*
 int X = 500;
@@ -370,11 +374,12 @@ shared_ptr<geometry::PointCloud> calc_filtered(shared_ptr<geometry::PointCloud> 
         double rawZ = -raw_pcd_ptr->points_[i][0];
 
         double r = sqrt(rawX * rawX + rawZ * rawZ);
-        double thetaVal = (theta - 500) / 1000.0;
-        double phiVal = (phi - 500) / 1000.0;
-        double xp = (rawX * cos(phiVal) - rawY * sin(phiVal)) * cos(thetaVal) - (rawZ * cos(phiVal) - rawY * sin(phiVal)) * sin(thetaVal);
-        double yp = rawY * cos(phiVal) + r * sin(phiVal);
-        double zp = (rawX * cos(phiVal) - rawY * sin(phiVal)) * sin(thetaVal) + (rawZ * cos(phiVal) - rawY * sin(phiVal)) * cos(thetaVal);
+        double rollVal = (roll - 500) / 1000.0;
+        double pitchVal = (pitch - 500) / 1000.0;
+        double yawVal = (yaw - 500) / 1000.0;
+        double xp = cos(yawVal) * cos(pitchVal) * rawX + (cos(yawVal) * sin(pitchVal) * sin(rollVal) - sin(yawVal) * cos(rollVal)) * rawY + (cos(yawVal) * sin(pitchVal) * cos(rollVal) + sin(yawVal) * sin(rollVal)) * rawZ;
+        double yp = sin(yawVal) * cos(pitchVal) * rawX + (sin(yawVal) * sin(pitchVal) * sin(rollVal) + cos(yawVal) * cos(rollVal)) * rawY + (sin(yawVal) * sin(pitchVal) * cos(rollVal) - cos(yawVal) * sin(rollVal)) * rawZ;
+        double zp = -sin(pitchVal) * rawX + cos(pitchVal) * sin(rollVal) * rawY + cos(pitchVal) * cos(rollVal) * rawZ;
         double x = xp + (X - 500) / 100.0;
         double y = yp + (Y - 500) / 100.0;
         double z = zp + (Z - 500) / 100.0;
@@ -433,6 +438,11 @@ shared_ptr<geometry::PointCloud> calc_filtered(shared_ptr<geometry::PointCloud> 
         // Find neighbors
         for (int i = 0; i + 1 < layer_cnt; i++)
         {
+            if (layers[i + 1].size() == 0)
+            {
+                continue;
+            }
+
             for (int j = 0; j < layers[i].size(); j++)
             {
                 int u = (int)(width / 2 + f_x * layers[i][j][0] / layers[i][j][2]);
@@ -535,7 +545,7 @@ shared_ptr<geometry::PointCloud> calc_filtered(shared_ptr<geometry::PointCloud> 
 
 double segmentate(int data_no, double color_segment_k, int color_size_min, double gaussian_sigma, double point_segment_k, int point_size_min, double color_rate, bool see_res = false)
 {
-    const string folder_path = "../../../data/2020_02_04_13jo/";
+    const string folder_path = "../../../data/2020_02_04_miyanosawa/";
     const string img_name = folder_path + to_string(data_no) + ".png";
     const string file_name = folder_path + to_string(data_no) + ".pcd";
     const bool vertical = true;
@@ -560,7 +570,9 @@ double segmentate(int data_no, double color_segment_k, int color_size_min, doubl
     vector<vector<double>> base_z, filtered_z;
     vector<vector<int>> neighbors;
     *pcd_ptr = pointcloud;
+    cout << data_no << endl;
     shared_ptr<geometry::PointCloud> filtered_ptr = calc_filtered(pcd_ptr, base_z, filtered_z, neighbors);
+    cout << data_no << endl;
 
     { // Coloring
         for (int i = 0; i < filtered_ptr->points_.size(); i++)
@@ -899,6 +911,7 @@ double segmentate(int data_no, double color_segment_k, int color_size_min, doubl
         cout << "cannot cnt = " << cannot_cnt - cnt << endl;
         cout << "Error = " << error << endl;
     }
+    ofs << data_no << "," << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << "," << error << "," << endl;
 
     if (see_res)
     {
@@ -918,12 +931,20 @@ double segmentate(int data_no, double color_segment_k, int color_size_min, doubl
 int main(int argc, char *argv[])
 {
     //vector<int> data_nos = {550, 1000, 1125, 1260, 1550}; // 03_03_miyanosawa
-    vector<int> data_nos = {10, 20, 30, 40, 50}; // 02_04_13jo
+    //vector<int> data_nos = {10, 20, 30, 40, 50}; // 02_04_13jo
     //vector<int> data_nos = {700, 1290, 1460, 2350, 3850}; // 02_04_miyanosawa
+
+    vector<int> data_nos;
+    for (int i = 1100; i < 1300; i++)
+    {
+        data_nos.emplace_back(i);
+    }
+
     for (int i = 0; i < data_nos.size(); i++)
     {
-        segmentate(data_nos[i], 0, 0, 0.5, 3, 3, 0, true);
+        segmentate(data_nos[i], 0, 0, 0.5, 3, 3, 0, false);
     }
+    return 0;
 
     double best_error = 100;
     double best_color_segment_k = 1;
