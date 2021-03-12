205ff8b2df5fa77a5e2ed7dbea2ec4a2a609772c
sono8stream <sono.stream8580@gmail.com>
2021-03-12 20:22:05 +0900

    Cannot fix gridding error

--- preprocess/grid_pointcloud.cpp
+++ preprocess/grid_pointcloud.cpp
@@ -11,10 +11,11 @@ using namespace std;
 /*
 Transform point cloud into depth image
 */
-void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ>& src_cloud,
+void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ> &src_cloud,
                      double min_angle_degree, double max_angle_degree,
-                     int target_layer_cnt, EnvParams& env_params, cv::Mat& grid,
-                     cv::Mat& vs) {
+                     int target_layer_cnt, EnvParams &env_params, cv::Mat &grid,
+                     cv::Mat &vs)
+{
   double PI = acos(-1);
   double min_rad = min_angle_degree * PI / 180;
   double delta_rad =
@@ -22,7 +23,6 @@ void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ>& src_cloud,
 
   grid = cv::Mat::zeros(target_layer_cnt, env_params.width, CV_64FC1);
   vs = cv::Mat::zeros(target_layer_cnt, env_params.width, CV_16SC1);
-  cv::Mat hoge = cv::Mat::zeros(env_params.height, env_params.width, CV_64FC1);
   cv::Mat vs2 = cv::Mat::zeros(target_layer_cnt, env_params.width, CV_64FC1);
 
   double rollVal = (env_params.roll - 500) / 1000.0;
@@ -39,7 +39,8 @@ void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ>& src_cloud,
       cos(pitchVal) * sin(rollVal), cos(pitchVal) * cos(rollVal),
       (env_params.Z - 500) / 100.0, 0, 0, 0, 1;
 
-  for (int i = 0; i < src_cloud.points.size(); i++) {
+  for (int i = 0; i < src_cloud.points.size(); i++)
+  {
     double rawX = src_cloud.points[i].x;
     double rawY = src_cloud.points[i].y;
     double rawZ = src_cloud.points[i].z;
@@ -54,20 +55,94 @@ void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ>& src_cloud,
 
     int v_idx = (int)((atan2(rawY, rawR) - min_rad) / delta_rad);
 
-    if (z > 0) {
+    if (z > 0)
+    {
       int u = (int)(env_params.width / 2 + env_params.f_xy * x / z);
       int v = (int)(env_params.height / 2 + env_params.f_xy * y / z);
-      if (0 <= u && u < env_params.width && 0 <= v && v < env_params.height) {
+      if (0 <= u && u < env_params.width && 0 <= v && v < env_params.height)
+      {
         grid.at<double>(v_idx, u) = z;
-        vs.at<ushort>(v_idx, u) = (ushort)v;
+        //vs.at<ushort>(v_idx, u) = (ushort)v;
         vs2.at<double>(v_idx, u) = v / 300.0;
-        hoge.at<double>(v, u) = z;
       }
     }
   }
 
-  vs.forEach<ushort>([&](ushort& now, const int position[]) -> void {
-    if (now > 0) {
+  pcl::PointCloud<pcl::PointXYZ> tst_cloud;
+  cv::Mat hoge = cv::Mat::zeros(env_params.height, env_params.width, CV_8UC1);
+  for (int i = 0; i < vs.rows; i++)
+  {
+    for (int j = 0; j < vs.cols; j++)
+    {
+      double rawY = tan(min_rad + i * delta_rad);
+      double A = env_params.f_xy * calibration_mtx(0, 0) +
+                 (env_params.width / 2 - j) * calibration_mtx(2, 0);
+      double B = env_params.f_xy * calibration_mtx(0, 1) +
+                 (env_params.width / 2 - j) * calibration_mtx(2, 1);
+      double C = env_params.f_xy * calibration_mtx(0, 2) +
+                 (env_params.width / 2 - j) * calibration_mtx(2, 2);
+      double D = env_params.f_xy * calibration_mtx(0, 3) +
+                 (env_params.width / 2 - j) * calibration_mtx(2, 3);
+      double E = B * rawY + D;
+
+      // 判別式
+      if (A * A + C * C - E * E < 0)
+      {
+        return;
+      }
+
+      double rawX1 = (-A * E - C * sqrt(A * A + C * C - E * E)) / (A * A + C * C);
+      double rawX2 = (-A * E + C * sqrt(A * A + C * C - E * E)) / (A * A + C * C);
+      double rawX = 0;
+      double rawZ = 1;
+      // Z >= 0は前提とする
+      if ((A * rawX1 + E) / C <= 0)
+      {
+        rawX = rawX1;
+      }
+      else
+      {
+        // このケースはほぼ無し
+        rawX = rawX2;
+      }
+      rawZ = sqrt(1 - rawX * rawX);
+
+      double x = calibration_mtx(0, 0) * rawX + calibration_mtx(0, 1) * rawY +
+                 calibration_mtx(0, 2) * rawZ + calibration_mtx(0, 3);
+      double y = calibration_mtx(1, 0) * rawX + calibration_mtx(1, 1) * rawY +
+                 calibration_mtx(1, 2) * rawZ + calibration_mtx(1, 3);
+      double z = calibration_mtx(2, 0) * rawX + calibration_mtx(2, 1) * rawY +
+                 calibration_mtx(2, 2) * rawZ + calibration_mtx(2, 3);
+
+      tst_cloud.points.push_back(pcl::PointXYZ(x, y, z));
+
+      int u = (int)(env_params.width / 2 + env_params.f_xy * x / z);
+      if (abs(u - j) > 1)
+      {
+        cout << i << " " << j << " : " << u << endl;
+      }
+      int v = (int)(env_params.height / 2 + env_params.f_xy * y / z);
+      if (0 <= u && u < env_params.width && 0 <= v && v < env_params.height)
+      {
+        hoge.at<int>(v, u) = i * 10;
+      }
+    }
+  }
+  cv::imshow("hoge", hoge);
+  cv::waitKey();
+  {
+    pcl::visualization::CloudViewer viewer("Point Cloud");
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr(
+        new pcl::PointCloud<pcl::PointXYZ>(tst_cloud));
+    viewer.showCloud(cloud_ptr);
+    while (!viewer.wasStopped())
+    {
+    }
+  }
+
+  vs.forEach<ushort>([&](ushort &now, const int position[]) -> void {
+    if (now > 0)
+    {
       return;
     }
 
@@ -96,6 +171,7 @@ void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ>& src_cloud,
         vs2.at<double>(position[0], position[1]) = now / 300.0;
         */
 
+    /* Second strategy
     double rawY = tan(min_rad + position[0] * delta_rad);
     double A = env_params.f_xy * calibration_mtx(0, 0) +
                (env_params.width / 2 - position[1]) * calibration_mtx(2, 0);
@@ -121,6 +197,67 @@ void grid_pointcloud(pcl::PointCloud<pcl::PointXYZ>& src_cloud,
       now = v;
       int uu = (int)(env_params.width / 2 + env_params.f_xy * x / z);
       vs2.at<double>(position[0], position[1]) = now / 300.0;
+    }
+    */
+
+    double rawY = tan(min_rad + position[0] * delta_rad);
+    double A = env_params.f_xy * calibration_mtx(0, 0) +
+               (env_params.width / 2 - position[1]) * calibration_mtx(2, 0);
+    double B = env_params.f_xy * calibration_mtx(0, 1) +
+               (env_params.width / 2 - position[1]) * calibration_mtx(2, 1);
+    double C = env_params.f_xy * calibration_mtx(0, 2) +
+               (env_params.width / 2 - position[1]) * calibration_mtx(2, 2);
+    double D = env_params.f_xy * calibration_mtx(0, 3) +
+               (env_params.width / 2 - position[1]) * calibration_mtx(2, 3);
+    double E = B * rawY + D;
+
+    // 判別式
+    if (A * A + C * C - E * E < 0)
+    {
+      return;
+    }
+
+    double rawX1 = (-A * E - C * sqrt(A * A + C * C - E * E)) / (A * A + C * C);
+    double rawX2 = (-A * E + C * sqrt(A * A + C * C - E * E)) / (A * A + C * C);
+    double rawX = 0;
+    double rawZ = 1;
+    // Z >= 0は前提とする
+    if ((A * rawX1 + E) / C <= 0)
+    {
+      rawX = rawX1;
+    }
+    else
+    {
+      rawX = rawX2;
+    }
+    rawZ = sqrt(1 - rawX * rawX);
+
+    double x = calibration_mtx(0, 0) * rawX + calibration_mtx(0, 1) * rawY +
+               calibration_mtx(0, 2) * rawZ + calibration_mtx(0, 3);
+    double y = calibration_mtx(1, 0) * rawX + calibration_mtx(1, 1) * rawY +
+               calibration_mtx(1, 2) * rawZ + calibration_mtx(1, 3);
+    double z = calibration_mtx(2, 0) * rawX + calibration_mtx(2, 1) * rawY +
+               calibration_mtx(2, 2) * rawZ + calibration_mtx(2, 3);
+    int v = (int)(env_params.height / 2 + env_params.f_xy * y / z);
+
+    double s = calibration_mtx(2, 0) * rawX + calibration_mtx(2, 1) * rawY + calibration_mtx(2, 2) * rawZ + calibration_mtx(2, 3);
+    v = (int)(((env_params.f_xy * calibration_mtx(1, 0) +
+                env_params.height / 2 * calibration_mtx(2, 0)) *
+                   rawX +
+               (env_params.f_xy * calibration_mtx(1, 1) +
+                env_params.height / 2 * calibration_mtx(2, 1)) *
+                   rawY +
+               (env_params.f_xy * calibration_mtx(1, 2) +
+                env_params.height / 2 * calibration_mtx(2, 2)) *
+                   rawZ +
+               (env_params.f_xy * calibration_mtx(1, 3) +
+                env_params.height / 2 * calibration_mtx(2, 3))) /
+              s);
+    if (0 <= v && v < env_params.height)
+    {
+      now = (ushort)v;
+      int uu = (int)(env_params.width / 2 + env_params.f_xy * x / z);
+      vs2.at<double>(position[0], position[1]) = v / 300.0;
     }
   });
 
